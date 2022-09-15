d86db21a23d9e585756eb7fdf84402e1d8f3da72
shihchengyen <shihcheng@nus.edu.sg>
2022-09-15 17:46:17 +0800

    Updated to work with cluster-config.yaml files instead of config files in ~/.parallelcluster

--- update_snapshot.sh
+++ update_snapshot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# expects cluster config to reside in ~/.parallelcluster/config
+# expects cluster-config.yaml to reside in ~/
 # to be run outside the master node, before shutting down the cluster
-# ~/.parallelcluster/config will be automatically repointed to the most updated snapshot
+# ~/cluster-config.yaml will be automatically repointed to the most updated snapshot
 
 # pass unique description to identify all your snapshots as first argument
 # pass the total number of snapshots (backups and current) to keep
@@ -25,23 +25,6 @@ if [[ -z "${snap_in_prog// }" ]]; then
 	exit 1
 fi
 
-echo "saving snapshot"
-a=2
-until [ $a -lt 1 ]
-do
-	status=$(aws ec2 describe-snapshots --filters Name=snapshot-id,Values=$snap_in_prog --query "Snapshots[].State" --output text)
-	if [ "$status" == "completed" ]
-	then
-		break
-	fi
-	if [ "$status" == "error" ]
-	then
-		echo "ERROR: snapshot failed to save, exiting"
-		exit 1
-	fi	
-done
-echo "snapshot saved"
-
 keep=$2
 all_snaps=($(aws ec2 describe-snapshots --owner self --filters Name=description,Values=$1 --query 'Snapshots[].[StartTime,SnapshotId]' --output text | sort -n | cut -f 2))
 total_count=${#all_snaps[@]}
@@ -59,12 +42,13 @@ fi
 echo "keeping max $keep snapshots"
 aws ec2 describe-snapshots --owner self --filters Name=description,Values=$1 --query 'Snapshots[].[StartTime,SnapshotId]' --output text | sort -n
 
-sub_n=$(grep -n snap- ~/.parallelcluster/config | cut -d : -f 1)
-head -$(($sub_n-1)) ~/.parallelcluster/config > ~/.parallelcluster/config.temp
-echo "ebs_snapshot_id = $most_recent" >> ~/.parallelcluster/config.temp
-tail -n +$(($sub_n+1)) ~/.parallelcluster/config >> ~/.parallelcluster/config.temp
+cp ~/cluster-config.yaml ~/cluster-config.yaml.bak
 
-mv ~/.parallelcluster/config.temp ~/.parallelcluster/config
+sub_n=$(grep -n snap- ~/cluster-config.yaml | cut -d : -f 1)
+head -$(($sub_n-1)) ~/cluster-config.yaml > ~/cluster-config.temp
+echo "      SnapshotId: $most_recent" >> ~/cluster-config.temp
+tail -n +$(($sub_n+1)) ~/cluster-config.yaml >> ~/cluster-config.temp
+
+mv ~/cluster-config.temp ~/cluster-config.yaml
 
 echo "config file updated"
-
